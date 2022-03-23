fd3d3f2bc3f5ef4e0966dfadfb03807511a9467c
thyme9877 <87171252+thyme9877@users.noreply.github.com>
2022-03-24 00:02:07 +0900

    Update FreeBoardController.java

--- demo/src/main/java/com/example/demo/freeboard/FreeBoardController.java
+++ demo/src/main/java/com/example/demo/freeboard/FreeBoardController.java
@@ -8,7 +8,10 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/freeboard/*")
@@ -18,12 +21,62 @@ public class FreeBoardController {
 	@Autowired
 	private FreeBoardLogic freeBoardLogic = null;
 	
-	@RequestMapping("getFreeBoardList")
-	public String getFreeBoardList(Model model) {
+	//TEST URL : http://localhost:9000/freeboard/getFreeBoardList
+	@GetMapping("getFreeBoardList")
+	public String getFreeBoardList(@RequestParam Map<String,Object> pMap, Model model) {
 		logger.info("getFreeBoardList 호출 성공");
 		List<Map<String,Object>> freeBoardList = null;
-		freeBoardList = freeBoardLogic.getFreeBoardList();
+		freeBoardList = freeBoardLogic.getFreeBoardList(pMap);
 		model.addAttribute("freeBoardList", freeBoardList);
 		return "freeboard/getFreeBoardList";		
 	}
+	
+	//TEST URL : http://localhost:9000/freeboard/openFreeBoardWriteForm
+	@GetMapping("openFreeBoardWriteForm")
+	public String openFreeBoardWrite() {
+		logger.info("openFreeBoardWrite 호출 성공");
+		return "freeboard/freeBoardWriteForm";
+	}
+	
+	@PostMapping("insertFreeBoard")
+	public String insertFreeBoard(@RequestParam Map<String,Object> pMap) {
+		logger.info("insertFreeBoard 호출 성공");
+		freeBoardLogic.insertFreeBoard(pMap);
+		return "redirect:getFreeBoardList";
+	}
+	
+	//TEST URL : http://localhost:9000/freeboard/getFreeBoardDetail?FREE_NUM=글번호
+	@GetMapping("getFreeBoardDetail")
+	public String getFreeBoardDetail(@RequestParam Map<String,Object> pMap, Model model) {
+		logger.info("getFreeBoardDetail 호출 성공");
+		List<Map<String,Object>> freeBoardDetail = null;
+		freeBoardDetail = freeBoardLogic.getFreeBoardDetail(pMap);
+		model.addAttribute("freeBoardDetail", freeBoardDetail);
+		return "freeboard/getFreeBoardDetail";		
+	}
+	
+	//TEST URL : http://localhost:9000/freeboard/openFreeBoardModifyForm?FREE_NUM=글번호 
+	@GetMapping("openFreeBoardModifyForm")
+	public String openFreeBoardModify(@RequestParam Map<String,Object> pMap, Model model) {
+		logger.info("openFreeBoardModify 호출 성공");
+		List<Map<String,Object>> freeBoardDetail = null;
+		freeBoardDetail = freeBoardLogic.getFreeBoardDetail(pMap);
+		model.addAttribute("freeBoardDetail", freeBoardDetail);
+		return "freeboard/freeBoardModifyForm";
+	}
+	
+	@PostMapping("updateFreeBoard")
+	public String updateFreeBoard(@RequestParam Map<String,Object> pMap) {
+		logger.info("updateFreeBoard 호출 성공");
+		freeBoardLogic.updateFreeBoard(pMap);
+		return "redirect:getFreeBoardList";
+	}
+	
+	//TEST URL : http://localhost:9000/freeboard/deleteFreeBoard?FREE_NUM=글번호&MEM_NUM=회원번호
+	@GetMapping("deleteFreeBoard")
+	public String deleteFreeBoard(@RequestParam Map<String,Object> pMap) {
+		logger.info("deleteFreeBoard 호출 성공");
+		freeBoardLogic.deleteFreeBoard(pMap);
+		return "redirect:getFreeBoardList";
+	}	
 }
