35192d236fd8efe4f9f9dd7379f3c8ec30391609
thyme9877 <87171252+thyme9877@users.noreply.github.com>
2022-04-03 03:04:39 +0900

    0403

--- demo/src/main/java/com/example/demo/freeboard/FreeBoardLogic.java
+++ demo/src/main/java/com/example/demo/freeboard/FreeBoardLogic.java
@@ -12,86 +12,63 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class FreeBoardLogic {
 	Logger logger = LogManager.getLogger(FreeBoardLogic.class);
-
+	
 	@Autowired
 	private FreeBoardDao freeBoardDao = null;
-
-	int total = 0;
-
+		
 	public List<Map<String, Object>> getFreeBoardList(Map<String, Object> pMap) {
-		logger.info("FreeBoardLogic getFreeBoardList 호출 성공"+pMap);
-		List<Map<String,Object>> freeBoardList = null;
-
-		int page = 0;
-		int pageSize = 0;
-		int start = 0;
-		int end = 0;
-
-		if(pMap.get("page") != null) {
-			page = Integer.parseInt(pMap.get("page").toString());
-		}
-		if(pMap.get("pageSize") != null) {
-			pageSize = Integer.parseInt(pMap.get("pageSize").toString());
-		}
-		logger.info("page : "+page+", pageSize : "+pageSize);
-
-		if(page > 0) {
-			start = ((page-1)*pageSize)+1;
-			end = page*pageSize;
-			pMap.put("start", start);
-			if(end > total) {
-				pMap.put("end", total);
-			}
-			else {
-				pMap.put("end", end);
-			}
-		}
-		logger.info("start : "+start+", end : "+end);
-
-		freeBoardList = freeBoardDao.getFreeBoardList(pMap);
-		return freeBoardList;
+		logger.info("FreeBoardLogic getFreeBoardList 호출 성공");				
+		return freeBoardDao.getFreeBoardList(pMap);
 	}
-
-	public int totalRecord(Map<String, Object> pMap) {
-		total = freeBoardDao.totalRecord(pMap);
-		return total;
+	
+	public List<Map<String, Object>> searchFreeBoard(String keyword) {
+		logger.info("FreeBoardLogic searchFreeBoard 호출 성공");
+		logger.info("FreeBoardLogic searchFreeBoard keyword"+keyword);
+		List<Map<String,Object>> list = null;
+		list = freeBoardDao.searchFreeBoard(keyword);
+		return list;		
+	}
+	
+	public List<Map<String, Object>> getFreeBoardDetail(int free_num) {
+		logger.info("FreeBoardLogic getFreeBoardDetail 호출 성공");
+		logger.info("FreeBoardLogic getFreeBoardDetail free_num"+free_num);
+		List<Map<String,Object>> freeBoardDetail = null;
+		freeBoardDetail = freeBoardDao.getFreeBoardDetail(free_num);		
+		return freeBoardDetail;
+	}
+	
+	public void updateFreeBoardViews(int free_num) {
+		logger.info("FreeBoardLogic updateFreeBoardViews 호출 성공");
+		freeBoardDao.updateFreeBoardViews(free_num);		
 	}
 
 	@Transactional //트랜잭션
-	public int insertFreeBoard(Map<String,Object> pMap) {
+	public void insertFreeBoard(Map<String, Object> pMap) throws Exception {
 		logger.info("FreeBoardLogic insertFreeBoard 호출 성공");
-		int result = 0;
-		int freeNum = 0;
-		freeNum = freeBoardDao.getFreeNum();
-		result = freeBoardDao.insertFreeBoard(pMap);
-
-		if(pMap.get("IMAGE_FILENAME") != null && pMap.get("IMAGE_FILENAME").toString().length() > 1) {
-			pMap.put("FREE_NUM", freeNum);
-			freeBoardDao.insertImageInfo(pMap);
-		}
-
-		return result;
+		logger.info("FreeBoardLogic insertFreeBoard pMap : "+pMap);			
+		freeBoardDao.insertFreeBoard(pMap);
+	}
+	
+	public void insertImageFileList(List<Map<String, Object>> list) {
+		freeBoardDao.insertImageFileList(list);		
 	}
 
-	public List<Map<String, Object>> getFreeBoardDetail(Map<String, Object> pMap) {
-		logger.info("FreeBoardLogic getFreeBoardDetail 호출 성공");
-		List<Map<String,Object>> freeBoardDetail = null;
-		freeBoardDetail = freeBoardDao.getFreeBoardDetail(pMap);
-		return freeBoardDetail;
-	}
-
-	public int updateFreeBoard(Map<String,Object> pMap) {
+	public int updateFreeBoard(Map<String, Object> pMap) {
 		logger.info("FreeBoardLogic updateFreeBoard 호출 성공");
+		logger.info("FreeBoardLogic updateFreeBoard pMap : "+pMap);
+						
 		int result = 0;
 		result = freeBoardDao.updateFreeBoard(pMap);
 		return result;
 	}
 
-	public int deleteFreeBoard(Map<String, Object> pMap) {
+	public int deleteFreeBoard(int free_num) {
 		logger.info("FreeBoardLogic deleteFreeBoard 호출 성공");
 		int result = 0;
-		result = freeBoardDao.deleteFreeBoard(pMap);
-		return result;
+		result = freeBoardDao.deleteFreeBoard(free_num);
+		return result;		
 	}
+
 }
+
 
