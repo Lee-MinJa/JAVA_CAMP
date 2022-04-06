95e91cc0292127375ea8b91c505fe7fd30e2bcc6
thyme9877 <87171252+thyme9877@users.noreply.github.com>
2022-04-07 00:20:41 +0900

    Delete demo directory

--- demo/src/main/java/com/example/demo/member/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Integer> {
-	//select * from member where mem_id = ?
-	public Member findByMemId(String memId);
-}
